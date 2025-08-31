ba3197fd86168689ba524d8fa4bc49750d0a3984
Arun Nagarkoti <nagarkotiarun420@gmail.com>
2025-08-31 21:32:49 +0545

    Polymorphism

--- OOP.py
+++ OOP.py
@@ -228,3 +228,48 @@ print(triangle.color)
 print(triangle.is_filled)
 print(f"THe triangle is {triangle.height}")
 triangle.desc()
+
+
+
+
+# polymorphism
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+    
+class Circle(Shape):
+    def __init__ (self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14* self.radius ** 2
+
+class Square(Shape):
+    def __init__(self,side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self. base = base
+        self. height = height
+
+    def area(self):
+        return self.base* self.height * 0.5
+
+class Pizza(Circle):
+    def __init__(self, toppings , radius):
+        super().__init__(radius)
+        self.toppings = toppings
+
+shapes = [Circle(4), Square(5), Triangle(6,7), Pizza('pepporoni', 15)]
+
+for shape in shapes:
+    print(shape.area())
