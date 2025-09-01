a591a6c3b29ed6068eb03f5c44b9658628ea58e4
Arun Nagarkoti <nagarkotiarun420@gmail.com>
2025-09-01 20:39:26 +0545

    Duck typing and Aggregation

--- OOP.py
+++ OOP.py
@@ -273,3 +273,39 @@ shapes = [Circle(4), Square(5), Triangle(6,7), Pizza('pepporoni', 15)]
 
 for shape in shapes:
     print(shape.area())
+    
+    
+
+# Aggregation = Represents a relationship where one object (the whole) contains references to one or 
+# more INDEPENDENT obects (the parts)
+
+class Library:
+    def __init__(self,name):
+        self.name = name
+        self.books = []
+
+    def add_book(self,book):
+        self.books.append(book)
+
+    def list_books(self):
+        return [f"{book.title} by {book.author}" for book in self.books]
+
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+
+library = Library("NYP library")
+
+book1 = Book("Harry porter","Haru puttar")
+book2 = Book("Magic emperor","Zhuo fan")
+book3 = Book("TBATE","Arthur")   
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+print (library.name)
+
+for book in library.list_books():
+    print(book)
