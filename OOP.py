e98816c606aed5295192b54a693e502789b67720
Arun Nagarkoti <nagarkotiarun420@gmail.com>
2025-09-01 22:48:00 +0545

    Nested class

--- OOP.py
+++ OOP.py
@@ -336,3 +336,43 @@ car1 = Car("Ford","Mustang", 500, 18)
 car2 = Car("Chevrolet","Corvette",679,19)
 print(car1.display_car())
 
+
+# Nested class =  Class inside a class
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+
+        def get_details(self):
+            return f"{self.name} {self.position}"
+            
+
+    def __init__ (self, company_name):
+        self.company_name = company_name
+        self.employees = []
+
+    def add_employee (self, name, position):
+        new_employee = self.Employee(name, position)
+        self.employees.append(new_employee)
+
+    def list_employee(self):
+        return [employee.get_details() for employee in self.employees]
+
+company1 = Company("Hello world")
+company2 = Company("Bye world")
+company1.add_employee("Eugene", "Manager")
+company1.add_employee("Spongebob","Cook")
+company1.add_employee("squidward","ship")
+
+company2.add_employee("Rem", "Assitant")
+company2.add_employee("John","Cook")
+company2.add_employee("jane","ship")
+
+for employee in company1.list_employee():
+    print(employee)
+
+for employee in company2.list_employee():
+    print(employee)
+
